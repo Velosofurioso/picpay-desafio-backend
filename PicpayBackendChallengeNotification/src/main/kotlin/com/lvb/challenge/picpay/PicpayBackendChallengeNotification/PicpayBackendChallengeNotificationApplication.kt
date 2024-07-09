01c3ea54d74ddb57e06d5d3d6f447990244e97b1
Lucas Veloso <l.brando815@gmail.com>
2024-07-09 20:58:27 -0300

    Add project picpay back notification

---
+++ PicpayBackendChallengeNotification/src/main/kotlin/com/lvb/challenge/picpay/PicpayBackendChallengeNotification/PicpayBackendChallengeNotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.lvb.challenge.picpay.PicpayBackendChallengeNotification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PicpayBackendChallengeNotificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<PicpayBackendChallengeNotificationApplication>(*args)
+}
---
+++ PicpayBackendChallengeNotification/src/test/kotlin/com/lvb/challenge/picpay/PicpayBackendChallengeNotification/PicpayBackendChallengeNotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lvb.challenge.picpay.PicpayBackendChallengeNotification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PicpayBackendChallengeNotificationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
